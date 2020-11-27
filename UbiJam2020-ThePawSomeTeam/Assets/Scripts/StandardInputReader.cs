aac1d92d1e56dd87f92b54795a98b49a7a145313
Alex Cline <clinealex10@gmail.com>
2020-11-27 15:04:11 -0600

    Player can move left and right

---
+++ UbiJam2020-ThePawSomeTeam/Assets/Scripts/StandardInputReader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class StandardInputReader : MonoBehaviour
+{
+    private CharacterMovement movement = null;
+
+    private float horizontalMovementInput = 0f;
+
+    private void Awake()
+    {
+        movement = GetComponent<CharacterMovement>();
+    }
+
+    public void Update()
+    {
+        movement.MoveHorizontally(horizontalMovementInput);
+    }
+
+    public void ReadHorizontalMovement(CallbackContext context)
+    {
+        horizontalMovementInput = context.ReadValue<float>();
+    }
+}
