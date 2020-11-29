c1f628840a41eec07fb1b9631af9cbff4bc58a83
Kris <krischkrasch@gmx.de>
2020-11-29 14:43:10 +0100

    UwU

---
+++ UbiJam2020-ThePawSomeTeam/Assets/Scripts/ToggleCredits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCredits : MonoBehaviour
+{
+    public static Action showCredits = delegate {  };
+    private bool triggere = false;
+    
+    public static void invokeCredits()
+    {
+        showCredits?.Invoke();
+    }
+
+    public void triggered()
+    {
+        if (!triggere)
+        {
+            GetComponent<Canvas>().enabled = true;
+            triggere = true;
+        }
+        
+    }
+}
